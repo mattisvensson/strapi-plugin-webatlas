33ea921000778da26b526e89f341ad9369404491
Matti Svensson <matti.s.58@web.de>
2024-04-27 11:42:25 +0200

    add url_route field to content types

--- server/register.ts
+++ server/register.ts
@@ -1,5 +1,19 @@
 import { Strapi } from '@strapi/strapi';
+import _ from 'lodash';
 
 export default ({ strapi }: { strapi: Strapi }) => {
-  // register phase
+  Object.values(strapi.contentTypes).forEach((contentType) => {
+    const { attributes } = contentType;
+
+    // TODO: Check if contentType is enabled in plugin settings
+    _.set(attributes, 'url_route', {
+      writable: true,
+      private: false,
+      configurable: false,
+      visible: false,
+      default: null,
+      type: 'json',
+    });
+  });
+
 };
