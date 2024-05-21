d76a8d0de993bc1f58eebd049a1dc8378dfef1f4
Matti Svensson <matti.svensson@ueberbit.de>
2024-05-21 23:44:07 +0200

    add new navitem content type

--- server/content-types/index.ts
+++ server/content-types/index.ts
@@ -2,8 +2,10 @@
 
 import route from "./route";
 import navigation from "./navigation";
+import navitem from "./navitem";
 
 export default {
   route,
   navigation,
+  navitem
 };
---
+++ server/content-types/navitem/schema.ts
@@ -0,0 +1,41 @@
+export default {
+  collectionName: "navitem",
+  info: {
+    singularName: "navitem",
+    pluralName: "navitems",
+    displayName: "Navigation Items",
+    name: "navitems"
+  },
+  options: {
+    increments: true,
+    comment: ""
+  },
+  pluginOptions: {
+    "content-manager": {
+      visible: true
+    },
+    "content-type-builder": {
+      visible: true
+    }
+  },
+  attributes: {
+    navigation: {
+      type: "relation",
+      relation: "oneToOne",
+      target: "plugin::url-routes.navigation",
+      configurable: false,
+    },
+    route: {
+      type: "relation",
+      relation: "oneToOne",
+      target: "plugin::url-routes.route",
+      configurable: false,
+    },
+    parent: {
+      type: "relation",
+      relation: "oneToOne",
+      target: "plugin::url-routes.navitem",
+      configurable: false,
+    },
+  }
+}
