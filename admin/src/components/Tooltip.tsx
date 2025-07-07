9688e4f796ea1d4bbff7a1ef7b6380732344ae69
Matti Svensson <matti.s.58@web.de>
2025-07-07 16:17:48 +0200

    add padding to Tooltip component

--- admin/src/components/Tooltip.tsx
+++ admin/src/components/Tooltip.tsx
@@ -1,8 +1,10 @@
 import { Information } from '@strapi/icons';
-import { Tooltip as StrapiTooltip }  from '@strapi/design-system';
+import { Tooltip as StrapiTooltip, Box }  from '@strapi/design-system';
 
 export default function Tooltip({ description }: { description: string }) {
-    return <StrapiTooltip description={description}>
-        <Information aria-hidden="true"/>
-    </StrapiTooltip>
+    return <Box paddingLeft={1} paddingRight={1}>
+        <StrapiTooltip description={description}>
+            <Information aria-hidden="true"/>
+        </StrapiTooltip>
+    </Box>
 }
