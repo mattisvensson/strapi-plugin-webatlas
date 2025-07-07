bc7af4fb3ea9ab88b5b45912d801c932d1afeb3c
Matti Svensson <matti.s.58@web.de>
2025-07-07 16:17:33 +0200

    migrate settings page

--- admin/src/pages/Settings/index.tsx
+++ admin/src/pages/Settings/index.tsx
@@ -1,33 +1,32 @@
 /*
  *
  * Settings
+ * This file contains the settings page for the Webatlas plugin in Strapi.
+ * It allows users to configure which content types are enabled for URL aliases and navigations,
+ * as well as setting default fields, URL alias patterns, and API fields for each content type. 
  *
- */
+*/
 
 import { useEffect, useState, useReducer } from 'react';
-import { CheckPermissions, useOverlayBlocker } from '@strapi/helper-plugin';
 import { Check } from '@strapi/icons';
-import { Button, HeaderLayout, Layout, ContentLayout, Box, Select, Option, Accordion, AccordionToggle, AccordionContent, AccordionGroup, Typography, Divider, TextInput } from '@strapi/design-system';
+import { Button, Box, Accordion, Field } from '@strapi/design-system';
+import { Layouts } from '@strapi/admin/strapi-admin'
+import { SingleSelect, SingleSelectOption, MultiSelect, MultiSelectOption } from '@strapi/design-system';
 import usePluginConfig from '../../hooks/usePluginConfig';
 import { ContentType, ConfigContentType, PluginConfig } from '../../../../types';
 import transformToUrl from '../../../../utils/transformToUrl';
-import Tooltip from '../../components/Tooltip';
-
+import Tooltip from '../../components/Tooltip'
 import useAllContentTypes from '../../hooks/useAllContentTypes';
 
-type Action = 
+type Action =
   | { type: 'SET_SELECTED_CONTENT_TYPES'; payload: ConfigContentType[] }
   | { type: 'SET_DEFAULT_FIELD'; payload: { ctUid: string; field: string } }
   | { type: 'SET_PATTERN'; payload: { ctUid: string; pattern: string } }
   | { type: 'SET_API_FIELD'; payload: { ctUid: string; apiField: string } };
 
-const noopFallback = () => {}
-
 const Settings = () => {
   const { data: config, setConfig } = usePluginConfig();
-  const [expandedID, setExpandedID] = useState<string | null>(null);
   const [settingsState, dispatch] = useReducer(reducer, config || { selectedContentTypes: [] });
-  const { lockApp = noopFallback, unlockApp = noopFallback } = useOverlayBlocker();
   const { contentTypes: allContentTypesData } = useAllContentTypes();
   const allContentTypes = allContentTypesData?.filter((ct: ContentType) => ct.isDisplayed);
   const [initialState, setInitialState] = useState(config || { selectedContentTypes: [] })
@@ -36,23 +35,22 @@ const Settings = () => {
     let updatedContentTypes
     switch (action.type) {
       case 'SET_SELECTED_CONTENT_TYPES':
-        setExpandedID(null)
         updatedContentTypes = action.payload.map(ct => {
           return settingsState.selectedContentTypes.find((cta: ConfigContentType) => cta.uid === ct.uid) || ct
         })
         return { ...settingsState, selectedContentTypes: updatedContentTypes };
       case 'SET_DEFAULT_FIELD':
-        updatedContentTypes = settingsState.selectedContentTypes.map(ct => 
+        updatedContentTypes = settingsState.selectedContentTypes.map(ct =>
           ct.uid === action.payload.ctUid ? { ...ct, default: action.payload.field } : ct
         );
         return { ...settingsState, selectedContentTypes: updatedContentTypes };
       case 'SET_PATTERN':
-        updatedContentTypes = settingsState.selectedContentTypes.map(ct => 
+        updatedContentTypes = settingsState.selectedContentTypes.map(ct =>
           ct.uid === action.payload.ctUid ? { ...ct, pattern: transformToUrl(action.payload.pattern) } : ct
         );
         return { ...settingsState, selectedContentTypes: updatedContentTypes };
       case 'SET_API_FIELD':
-        updatedContentTypes = settingsState.selectedContentTypes.map(ct => 
+        updatedContentTypes = settingsState.selectedContentTypes.map(ct =>
           ct.uid === action.payload.ctUid ? { ...ct, apiField: transformToUrl(action.payload.apiField) } : ct
         );
         return { ...settingsState, selectedContentTypes: updatedContentTypes };
@@ -67,7 +65,6 @@ const Settings = () => {
 
   async function save() {
     if (!settingsState || settingsState.selectedContentTypes.find((cta: ConfigContentType) => !cta.default) !== undefined) return
-    lockApp();
 
     try {
       setConfig(settingsState)
@@ -75,8 +72,6 @@ const Settings = () => {
     } catch (err) {
       console.error(err);
     }
-
-    unlockApp()
   }
 
   useEffect(() => {
@@ -84,29 +79,26 @@ const Settings = () => {
     dispatch({ type: 'SET_SELECTED_CONTENT_TYPES', payload: config.selectedContentTypes });
   }, [config]);
 
-  const handleToggle = (id: string) => () => {
-    setExpandedID(s => s === id ? null : id);
-  };
-
   return (
-    <Layout>
-      <HeaderLayout
+    <>
+      <Layouts.Header
         title='Routes'
         subtitle='Settings'
         primaryAction={
-          <CheckPermissions permissions={false}>
-            <Button
-              type="submit"
-              startIcon={<Check />}
-              onClick={save}
-              disabled={JSON.stringify(settingsState) === JSON.stringify(initialState) || settingsState.selectedContentTypes.find((cta: ConfigContentType) => !cta.default) !== undefined}
-            >
-              Save
-            </Button>
-          </CheckPermissions>
+          <Button
+            type="submit"
+            startIcon={<Check />}
+            onClick={save}
+            disabled={
+              JSON.stringify(settingsState) === JSON.stringify(initialState)
+              || settingsState.selectedContentTypes.find((cta: ConfigContentType) => !cta.default) !== undefined
+            }
+          >
+            Save
+          </Button>
         }
       />
-      <ContentLayout>
+      <Layouts.Content>
         <Box
           background='neutral0'
           borderColor="neutral150"
@@ -117,80 +109,111 @@ const Settings = () => {
           paddingTop={6}
           shadow="tableShadow"
         >
-          <Select
-            name="selectedContentTypes"
-            label='Enabled Content Types'
-            hint='Select the content types you want to enable for URL aliases and navigations'
-            onClear={() => dispatch({ type: 'SET_SELECTED_CONTENT_TYPES', payload: [] })}
-            value={[...settingsState.selectedContentTypes.map((ct: ConfigContentType) => ct.uid)]}
-            onChange={(value: string[]) => dispatch({ type: 'SET_SELECTED_CONTENT_TYPES', payload: value.map(v => ({ uid: v })) })}
-            multi
-            withTags
-            disabled={false}
-          >
-            {allContentTypes && allContentTypes.map(item => <Option key={item.uid} value={item.uid}>{item.info.displayName}</Option>)}
-          </Select>
+          <Field.Root name="selectedContentTypes">
+            <Field.Label>Enabled Content Types</Field.Label>
+            <MultiSelect
+              placeholder='Select Content Types'
+              onClear={() => dispatch({ type: 'SET_SELECTED_CONTENT_TYPES', payload: [] })}
+              value={[...settingsState.selectedContentTypes.map((ct: ConfigContentType) => ct.uid)]}
+              onChange={(value: string[]) =>
+                dispatch({
+                  type: 'SET_SELECTED_CONTENT_TYPES',
+                  payload: value.map(v => ({
+                    uid: v,
+                    default: '',
+                    pattern: '',
+                    apiField: '',
+                  })),
+                })
+              }
+              withTags
+            >
+              {allContentTypes && allContentTypes.map(item => <MultiSelectOption key={item.uid} value={item.uid}>{item.info.displayName}</MultiSelectOption>)}
+            </MultiSelect>
+            <Field.Hint>
+              Select the content types you want to enable for URL aliases and navigations
+            </Field.Hint>
+          </Field.Root>
           {settingsState.selectedContentTypes && settingsState.selectedContentTypes.length > 0 && <Box paddingTop={4}>
-            <AccordionGroup label="Content Type settings">
-              {settingsState.selectedContentTypes?.map((contentType: ConfigContentType) => {
-                const ct: ContentType | undefined = allContentTypes?.find((item) => item.uid === contentType.uid)
-                if (!ct) return null
-                return (
-                  <Accordion key={ct.uid} expanded={expandedID === ct.uid} onToggle={handleToggle(ct.uid)} id={ct.uid} size="S">
+            <Field.Root name="selectedContentTypesAccordion">
+              <Field.Label>Enabled Content Types Settings</Field.Label>
+              <Accordion.Root label="Content Type settings">
+                {settingsState.selectedContentTypes?.map((contentType: ConfigContentType) => {
+                  const ct: ContentType | undefined = allContentTypes?.find((item) => item.uid === contentType.uid)
+                  if (!ct) return null
+                  return (
                     <Box borderColor={!contentType.default && 'danger500'}>
-                      <AccordionToggle title={ct?.info.displayName} togglePosition="left" />
+                      <Accordion.Item key={ct.uid} value={ct.uid} size="S">
+                        <Accordion.Header>
+                          <Accordion.Trigger>
+                            {ct?.info.displayName}
+                          </Accordion.Trigger>
+                        </Accordion.Header>
+                        <Accordion.Content>
+                          <Box padding={3}>
+                            <Field.Root name="electedContentTypes" error={!contentType.default && 'Please select a default field'} required>
+                              <Field.Label>Default URL Alias field</Field.Label>
+                              <SingleSelect
+                                name={`defaultField-${ct.uid}`}
+                                onClear={() => dispatch({ type: 'SET_DEFAULT_FIELD', payload: { ctUid: ct.uid, field: '' } })}
+                                value={contentType?.default || ''}
+                                onChange={(value: string) => dispatch({ type: 'SET_DEFAULT_FIELD', payload: { ctUid: ct.uid, field: value } })}
+                              >
+                                {Object.entries(ct.attributes).map(([key], index) => {
+                                  if (key === 'id' || key === 'createdAt' || key === 'updatedAt' || key === 'createdBy' || key === 'updatedBy') return null
+                                  return <SingleSelectOption key={index} value={key}>{key}</SingleSelectOption>
+                                })}
+                              </SingleSelect>
+                              <Field.Hint>
+                                The selected field will be used to generate the URL alias. Use a field that is unique and descriptive, such as a "title" or "name".
+                              </Field.Hint>
+                            </Field.Root>
+                            <Box paddingTop={4}>
+                              <Field.Root name="urlAliasPattern" hint='The selected field will be used to generate the URL alias. Use a field that is unique and descriptive, such as a "title" or "name".'>
+                                <Field.Label>
+                                  URL Alias pattern
+                                  <Tooltip description="Leading and trailing slashes will be removed. Spaces will be replaced with hyphens. Special characters will be encoded." />
+                                </Field.Label>
+                                <Field.Input
+                                  value={contentType.pattern}
+                                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => dispatch({ type: 'SET_PATTERN', payload: { ctUid: ct.uid, pattern: e.target.value } })}
+                                  disabled={!contentType.default}
+                                  type="text"
+                                  placeholder="e.g. blog"
+                                />
+                                <Field.Hint />
+                              </Field.Root>
+                            </Box>
+                            <Box paddingTop={4}>
+                              <Field.Root name="urlAliasPattern" hint='This field is necessary for the API. Do not change this field unless you know what you are doing.'>
+                                <Field.Label>
+                                  URL Alias API field
+                                  <Tooltip description="The field which will be visible in the content manger to store the url alias. Necessary for the API." />
+                                </Field.Label>
+                                <Field.Input
+                                  value={contentType.apiField}
+                                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => dispatch({ type: 'SET_API_FIELD', payload: { ctUid: ct.uid, apiField: e.target.value } })}
+                                  disabled={!contentType.default}
+                                  type="text"
+                                  placeholder="e.g. teaser.teaser_url"
+                                />
+                                <Field.Hint />
+                              </Field.Root>
+                            </Box>
+                          </Box>
+                        </Accordion.Content>
+                      </Accordion.Item>
                     </Box>
-                    <AccordionContent>
-                      <Box padding={3}>
-                        <Select
-                          required
-                          error={!contentType.default && 'Please select a default field'}
-                          name={`defaultField-${ct.uid}`}
-                          label='Default URL Alias field'
-                          hint='The selected field will be used to generate the URL alias. Use a field that is unique and descriptive, such as a "title" or "name".'
-                          onClear={() => dispatch({ type: 'SET_DEFAULT_FIELD', payload: { ctUid: ct.uid, field: '' } })}
-                          value={contentType?.default || ''}
-                          onChange={(value: string) => dispatch({ type: 'SET_DEFAULT_FIELD', payload: { ctUid: ct.uid, field: value } })}
-                        >
-                          {Object.entries(ct.attributes).map(([key], index) => {
-                            if (key === 'id' || key === 'createdAt' || key === 'updatedAt' || key === 'createdBy' || key === 'updatedBy') return null
-                            return <Option key={index} value={key}>{key}</Option>
-                          })}
-                        </Select>
-                        <Box paddingTop={4}>
-                          <TextInput
-                            label="URL Alias pattern"
-                            placeholder="e.g. blog"
-                            labelAction={<Tooltip description="Leading and trailing slashes will be removed. Spaces will be replaced with hyphens. Special characters will be encoded."/>}
-                            hint="Define the pattern for the URL alias. The default field will be appended to this pattern."
-                            value={contentType.pattern}
-                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => dispatch({ type: 'SET_PATTERN', payload: { ctUid: ct.uid, pattern: e.target.value } })}                
-                            disabled={!contentType.default}
-                          />
-                        </Box>
-                        <Box paddingTop={4}>
-                          <TextInput
-                            label="URL Alias API field"
-                            placeholder="e.g. teaser.teaser_url"
-                            labelAction={<Tooltip description="The field which will be visible in the content manger to store the url alias. Necessary for the API."/>}
-                            hint="This field is necessary for the API. Do not change this field unless you know what you are doing."
-                            value={contentType.apiField}
-                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => dispatch({ type: 'SET_API_FIELD', payload: { ctUid: ct.uid, apiField: e.target.value } })}                
-                            disabled={!contentType.default}
-                          />
-                        </Box>
-                      </Box>
-                    </AccordionContent>
-                  </Accordion>
-                )
-              })}
-            </AccordionGroup>
+                  )
+                })}
+              </Accordion.Root>
+            </Field.Root>
           </Box>}
         </Box>
         <Box>
         </Box>
-      </ContentLayout>
-    </Layout>
+      </Layouts.Content>
+    </>
   );
 };
 
