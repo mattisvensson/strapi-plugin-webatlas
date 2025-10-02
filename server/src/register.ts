ec25146770571f710ac6f7c55a0f319eaba6cd48
Matti Svensson <matti.s.58@web.de>
2025-10-02 21:54:54 +0200

    add webatlas fields to contentTypes on register

--- server/src/register.ts
+++ server/src/register.ts
@@ -1,7 +1,30 @@
-import type { Core } from '@strapi/strapi';
+import set from 'lodash/set';
+import { Core, Schema } from '@strapi/strapi';
 
-const register = ({ strapi }: { strapi: Core.Strapi }) => {
-  // register phase
+export default ({ strapi }: { strapi: Core.Strapi }) => {
+  Object.values(strapi.contentTypes).forEach((contentType: Schema.ContentType) => {
+    
+    // TODO: Only add fields to content types that are allowed to have routes
+    if (!contentType.uid.startsWith('api::')) return;
+      
+    const { attributes } = contentType;
+
+    const fieldSettings = {
+      writable: true,
+      private: true,
+      configurable: false,
+      editable: false,
+      visible: true,
+      default: null,
+    }
+
+    set(attributes, 'webatlas_path', {
+      ...fieldSettings,
+      type: 'string',
+    });    
+    set(attributes, 'webatlas_override', {
+      ...fieldSettings,
+      type: 'boolean',
+    });    
+  });
 };
-
-export default register;
