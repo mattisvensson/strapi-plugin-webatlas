0ff6d65844ebb3b85fe462cb29c8f6a9911f8665
Matti Svensson <matti.s.58@web.de>
2025-10-27 23:45:04 +0100

    fix: update routes page, add empty screen

--- admin/src/pages/Routes/index.tsx
+++ admin/src/pages/Routes/index.tsx
@@ -7,11 +7,12 @@
 */
 
 import { useState, useEffect } from 'react';
-import { useFetchClient, Layouts } from '@strapi/strapi/admin';
+import { Layouts } from '@strapi/strapi/admin';
 import { Flex, Typography, Table, Thead, Tbody, Tr, Td, Th, VisuallyHidden, LinkButton } from '@strapi/design-system';
 import { Pencil } from '@strapi/icons';
 import { Route } from '../../../../types';
 import { useApi } from '../../hooks';
+import { EmptyBox, Center } from '../../components/UI';
 
 const Routes = () => {
   const { getRoutes } = useApi();
@@ -33,55 +34,50 @@ const Routes = () => {
         subtitle='Overview of all existing routes'
       />
       <Layouts.Content>
-        <Table colCount={6} rowCount={routes.length}>
-          <Thead>
-            <Tr>
-              <Th>
-                <Typography variant="sigma">ID</Typography>
-              </Th>
-              <Th>
-                <Typography variant="sigma">Title</Typography>
-              </Th>
-              <Th>
-                <Typography variant="sigma">Route</Typography>
-              </Th>
-              <Th>
-                <Typography variant="sigma">Internal</Typography>
-              </Th>
-              <Th>
-                <Typography variant="sigma">Active</Typography>
-              </Th>
-              <Th>
-                <VisuallyHidden>Actions</VisuallyHidden>
-              </Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {routes.map((route: Route) => <Tr key={route.id}>
-              <Td>
-                <Typography textColor="neutral800">{route.id}</Typography>
-              </Td>
-              <Td>
-                <Typography textColor="neutral800">{route.title}</Typography>
-              </Td>
-              <Td>
-                <Typography textColor="neutral800">{route.fullPath}</Typography>
-              </Td>
-              <Td>
-                <Typography textColor="neutral800">{route.internal ? 'Yes' : 'No'}</Typography>
-              </Td>
-              <Td>
-                <Typography textColor="neutral800">{route.active ? 'Yes' : 'No'}</Typography>
-              </Td>
-              <Td>
-                <Flex gap={2} justifyContent="end">
-                  {/* <Button variant="secondary">More info</Button> */}
-                  <LinkButton variant="secondary" startIcon={<Pencil />} href={`/admin/content-manager/collection-types/${route.relatedContentType}/${route.relatedId}`}>Edit</LinkButton>
-                </Flex>
-              </Td>
-            </Tr>)}
-          </Tbody>
-        </Table>
+        {routes.length === 0 ? (
+          <Center height={400}>
+            <EmptyBox msg="No routes found" />
+          </Center>
+        ) : (
+          <Table colCount={4} rowCount={routes.length}>
+            <Thead>
+              <Tr>
+                <Th>
+                  <Typography variant="sigma">ID</Typography>
+                </Th>
+                <Th>
+                  <Typography variant="sigma">Title</Typography>
+                </Th>
+                <Th>
+                  <Typography variant="sigma">Route</Typography>
+                </Th>
+                <Th>
+                  <VisuallyHidden>Actions</VisuallyHidden>
+                </Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {routes.map((route: Route) => (
+                <Tr key={route.id}>
+                  <Td>
+                    <Typography textColor="neutral800">{route.id}</Typography>
+                  </Td>
+                  <Td>
+                    <Typography textColor="neutral800">{route.title}</Typography>
+                  </Td>
+                  <Td>
+                    <Typography textColor="neutral800">{route.fullPath}</Typography>
+                  </Td>
+                  <Td>
+                    <Flex gap={2} justifyContent="end">
+                      <LinkButton variant="secondary" startIcon={<Pencil />} href={`/admin/content-manager/collection-types/${route.relatedContentType}/${route.relatedDocumentId}`}>Edit</LinkButton>
+                    </Flex>
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        )}
       </Layouts.Content>
     </>
   );
