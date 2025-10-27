e41c4c71e8ce5c5e85b1a5732f0fa7bcc41f5da8
Matti Svensson <matti.s.58@web.de>
2025-10-27 22:55:13 +0100

    fix: use radix tooltip instead of strapi design system

--- admin/src/components/Tooltip.tsx
+++ admin/src/components/Tooltip.tsx
@@ -1,10 +1,32 @@
+
+import * as RadixTooltip from '@radix-ui/react-tooltip';
 import { Information } from '@strapi/icons';
-import { Tooltip as StrapiTooltip, Box }  from '@strapi/design-system';
+import { Box, Typography }  from '@strapi/design-system';
+
+// Since there occurs an error with the Strapi design system Tooltip component,
+// we use the Radix Tooltip component directly here.
+// see https://github.com/strapi/strapi/issues/21823
 
 export default function Tooltip({ description }: { description: string }) {
-    return <Box paddingLeft={1} paddingRight={1}>
-        <StrapiTooltip label={description}>
-            <Information aria-hidden="true"/>
-        </StrapiTooltip>
-    </Box>
+    return (
+        <RadixTooltip.Provider>
+            <RadixTooltip.Root>
+                <RadixTooltip.Trigger asChild>
+                    <Information aria-hidden="true"/>
+                </RadixTooltip.Trigger>
+                <RadixTooltip.Portal>
+                    <RadixTooltip.Content sideOffset={5}>
+                        <Box padding={2} margin={2} background="neutral1000"
+                            hasRadius
+                            shadow="filterShadow">
+                            <Typography textColor="neutral0">
+                                {description}
+                            </Typography>
+                        </Box>
+                    </RadixTooltip.Content>
+                </RadixTooltip.Portal>
+            </RadixTooltip.Root>
+        </RadixTooltip.Provider>
+    )
 }
+
