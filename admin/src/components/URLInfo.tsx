4240278742a468ccbba218163e8762976509a954
Matti Svensson <matti.s.58@web.de>
2025-11-10 12:14:31 +0100

    feature: add translations for URLInfo component

--- admin/src/components/URLInfo.tsx
+++ admin/src/components/URLInfo.tsx
@@ -1,4 +1,6 @@
 import { Box, Typography } from '@strapi/design-system';
+import { getTranslation } from '../utils';
+import { useIntl } from 'react-intl';
 
 interface URLInfoProps {
   validationState: 'initial' | 'checking' | 'done';
@@ -6,16 +8,33 @@ interface URLInfoProps {
 }
 
 export default function URLInfo({validationState, replacement}: URLInfoProps) {
+
+	const { formatMessage } = useIntl();
+
 	if (validationState === 'initial') return null
 	let color = 'neutral800'
-	let text = 'Checking if URL is available...'
+	let text = formatMessage({
+		id: getTranslation('components.URLInfo.checking'),
+		defaultMessage: 'Checking if URL is available...',
+	})
 
 	if (validationState === 'checking') {
 		color = 'neutral800'
-		text = 'Checking if URL is available...'
+		text = formatMessage({
+			id: getTranslation('components.URLInfo.checking'),
+			defaultMessage: 'Checking if URL is available...',
+		})
 	} else if (validationState === 'done') {
 		color = replacement ? 'danger500' : 'success500'
-		text = replacement ? `URL is not available. Replaced with "${replacement}".` : 'URL is available'
+		text = replacement ? 
+			`${formatMessage({
+				id: getTranslation('components.URLInfo.notAvailable'),
+				defaultMessage: 'URL is not available. Replaced with',
+			})} "${replacement}".` : 
+			formatMessage({
+				id: getTranslation('components.URLInfo.available'),
+				defaultMessage: 'URL is available.',
+			})
 	}
 
 	return (
