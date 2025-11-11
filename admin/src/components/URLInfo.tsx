42f2ef06c19bbdd26f2f7f2fb16288cd2ae7019a
Matti Svensson <matti.s.58@web.de>
2025-11-11 18:04:03 +0100

    fix: add validation callback to URLInfo component

--- admin/src/components/URLInfo.tsx
+++ admin/src/components/URLInfo.tsx
@@ -1,41 +1,41 @@
 import { Box, Typography } from '@strapi/design-system';
 import { getTranslation } from '../utils';
 import { useIntl } from 'react-intl';
+import { useState, useEffect } from 'react';
 
 interface URLInfoProps {
   validationState: 'initial' | 'checking' | 'done';
 	replacement: string;
+	setUrlStatus?: (isValid: 'valid' | 'invalid') => void;
 }
 
-export default function URLInfo({validationState, replacement}: URLInfoProps) {
-
+export default function URLInfo({validationState, replacement, setUrlStatus}: URLInfoProps) {
+	const [color, setColor] = useState<string | null>(null);
+	const [text, setText] = useState<string | null>(null);
 	const { formatMessage } = useIntl();
 
-	if (validationState === 'initial') return null
-	let color = 'neutral800'
-	let text = formatMessage({
-		id: getTranslation('components.URLInfo.checking'),
-		defaultMessage: 'Checking if URL is available...',
-	})
-
-	if (validationState === 'checking') {
-		color = 'neutral800'
-		text = formatMessage({
-			id: getTranslation('components.URLInfo.checking'),
-			defaultMessage: 'Checking if URL is available...',
-		})
-	} else if (validationState === 'done') {
-		color = replacement ? 'danger500' : 'success500'
-		text = replacement ? 
-			`${formatMessage({
-				id: getTranslation('components.URLInfo.notAvailable'),
-				defaultMessage: 'URL is not available. Replaced with',
-			})} "${replacement}".` : 
-			formatMessage({
-				id: getTranslation('components.URLInfo.available'),
-				defaultMessage: 'URL is available.',
-			})
-	}
+	useEffect(() => {
+		if (validationState === 'initial') return
+		if (validationState === 'checking') {
+			setColor('neutral800')
+			setText(formatMessage({
+				id: getTranslation('components.URLInfo.checking'),
+				defaultMessage: 'Checking if URL is available...',
+			}))
+		} else if (validationState === 'done') {
+			setColor(replacement ? 'danger500' : 'success500')
+			setText(replacement ? 
+				`${formatMessage({
+					id: getTranslation('components.URLInfo.notAvailable'),
+					defaultMessage: 'URL is not available. Replaced with',
+				})} "${replacement}".` : 
+				formatMessage({
+					id: getTranslation('components.URLInfo.available'),
+					defaultMessage: 'URL is available.',
+				}))
+			if (setUrlStatus) setUrlStatus(replacement ? 'invalid' : 'valid');
+		}
+	}, [validationState, replacement, formatMessage, setUrlStatus]);
 
 	return (
 		<Box paddingTop={2}>
