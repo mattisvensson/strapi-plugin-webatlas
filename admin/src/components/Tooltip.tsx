6043759ae662ed22f103008a00d2ecbf4df0bc74
Matti Svensson <matti.s.58@web.de>
2025-11-11 17:36:02 +0100

    fix: add maxWidth to tooltip component

--- admin/src/components/Tooltip.tsx
+++ admin/src/components/Tooltip.tsx
@@ -1,32 +1,37 @@
 
 import * as RadixTooltip from '@radix-ui/react-tooltip';
 import { Information } from '@strapi/icons';
-import { Box, Typography }  from '@strapi/design-system';
+import { Box, Typography } from '@strapi/design-system';
 
 // Since there occurs an error with the Strapi design system Tooltip component,
 // we use the Radix Tooltip component directly here.
 // see https://github.com/strapi/strapi/issues/21823
 
 export default function Tooltip({ description }: { description: string }) {
-    return (
-        <RadixTooltip.Provider>
-            <RadixTooltip.Root>
-                <RadixTooltip.Trigger asChild>
-                    <Information aria-hidden="true"/>
-                </RadixTooltip.Trigger>
-                <RadixTooltip.Portal>
-                    <RadixTooltip.Content sideOffset={5}>
-                        <Box padding={2} margin={2} background="neutral1000"
-                            hasRadius
-                            shadow="filterShadow">
-                            <Typography textColor="neutral0">
-                                {description}
-                            </Typography>
-                        </Box>
-                    </RadixTooltip.Content>
-                </RadixTooltip.Portal>
-            </RadixTooltip.Root>
-        </RadixTooltip.Provider>
-    )
+	return (
+		<RadixTooltip.Provider>
+			<RadixTooltip.Root>
+				<RadixTooltip.Trigger asChild>
+					<Information aria-hidden="true" />
+				</RadixTooltip.Trigger>
+				<RadixTooltip.Portal>
+					<RadixTooltip.Content sideOffset={5}>
+						<Box
+							padding={2}
+							margin={2}
+							background="neutral1000"
+							hasRadius
+							shadow="filterShadow"
+							maxWidth="300px"
+						>
+							<Typography textColor="neutral0">
+								{description}
+							</Typography>
+						</Box>
+					</RadixTooltip.Content>
+				</RadixTooltip.Portal>
+			</RadixTooltip.Root>
+		</RadixTooltip.Provider>
+	)
 }
 
