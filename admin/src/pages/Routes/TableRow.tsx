ce5e0d952c9839eee6a373dc64455edcc01a2d77
Matti Svensson <matti.s.58@web.de>
2025-11-12 23:24:35 +0100

    refactor routes page and add notifications

---
+++ admin/src/pages/Routes/TableRow.tsx
@@ -0,0 +1,47 @@
+import type { Route } from '../../../../types';
+import { Typography, Tr, Td, Flex, LinkButton } from '@strapi/design-system';
+import { getTranslation, getRouteType } from '../../utils';
+import { useIntl } from 'react-intl';
+import { Pencil } from '@strapi/icons';
+
+export default function TableRow({ route }: { route: Route }) {
+  const { formatMessage } = useIntl();
+  
+  return (
+    <Tr>
+      <Td>
+        <Typography textColor="neutral800">{route.id}</Typography>
+      </Td>
+      <Td>
+        <Typography textColor="neutral800">{route.title}</Typography>
+      </Td>
+      <Td>
+        <Typography textColor="neutral800">{route.fullPath}</Typography>
+      </Td>
+      <Td>
+        <Typography textColor="neutral800">
+          {formatMessage({
+            id: getTranslation(`route.type.${getRouteType(route)}`),
+            defaultMessage: '-',
+          })}
+        </Typography>
+      </Td>
+      <Td>
+        <Flex gap={2} justifyContent="end">
+          {route.internal && 
+            <LinkButton
+              variant="secondary"
+              startIcon={<Pencil />} 
+              href={`/admin/content-manager/collection-types/${route.relatedContentType}/${route.relatedDocumentId}`}
+            >
+              {formatMessage({
+                id: getTranslation('edit'),
+                defaultMessage: 'Edit',
+              })}
+            </LinkButton>
+          }
+        </Flex>
+      </Td>
+    </Tr>
+  )
+}
