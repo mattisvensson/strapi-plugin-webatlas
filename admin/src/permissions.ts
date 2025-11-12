5024d4f965c2b4eee76a3d16a0ae076dd2d5c7e0
Matti Svensson <matti.s.58@web.de>
2025-11-12 12:00:03 +0100

    feature: add basic rbac for webatlas pages

---
+++ admin/src/permissions.ts
@@ -0,0 +1,7 @@
+const pluginPermissions = {
+  'page.navigation': [{ action: 'plugin::webatlas.page.navigation', subject: null }],
+  'page.routes': [{ action: 'plugin::webatlas.page.routes', subject: null }],
+  'settings.configuration': [{ action: 'plugin::webatlas.settings.configuration', subject: null }],
+};
+
+export default pluginPermissions;
