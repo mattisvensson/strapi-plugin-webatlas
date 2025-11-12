43f79edd577721353b950663c6ebd18afbc95ef8
Matti Svensson <matti.s.58@web.de>
2025-11-12 21:29:48 +0100

    refactor settings page code

--- admin/src/pages/Settings/index.tsx
+++ admin/src/pages/Settings/index.tsx
@@ -10,16 +10,16 @@
 import { useEffect, useState, useReducer } from 'react';
 import { Box, Accordion, Field } from '@strapi/design-system';
 import { useNotification } from '@strapi/strapi/admin'
-import { SingleSelect, SingleSelectOption, MultiSelect, MultiSelectOption } from '@strapi/design-system';
+import { MultiSelect, MultiSelectOption } from '@strapi/design-system';
 import usePluginConfig from '../../hooks/usePluginConfig';
 import type { ContentType, ConfigContentType, PluginConfig } from '../../../../types';
 import transformToUrl from '../../../../utils/transformToUrl';
-import Tooltip from '../../components/Tooltip'
 import useAllContentTypes from '../../hooks/useAllContentTypes';
 import { getTranslation } from '../../utils';
 import { useIntl } from 'react-intl';
 import { FullLoader } from '../../components/UI';
 import PageWrapper from './PageWrapper';
+import ContentTypeAccordion from './ContentTypeAccordion';
 
 type Action =
   | { type: 'SET_SELECTED_CONTENT_TYPES'; payload: ConfigContentType[] }
@@ -148,7 +148,9 @@ const Settings = () => {
           }
           withTags
         >
-          {allContentTypes && allContentTypes.map(item => <MultiSelectOption key={item.uid} value={item.uid}>{item.info.displayName}</MultiSelectOption>)}
+          {allContentTypes && allContentTypes.map(item => 
+            <MultiSelectOption key={item.uid} value={item.uid}>{item.info.displayName}</MultiSelectOption>
+          )}
         </MultiSelect>
         <Field.Hint/>
       </Field.Root>
@@ -162,98 +164,9 @@ const Settings = () => {
               })}
             </Field.Label>
             <Accordion.Root>
-              {settingsState.selectedContentTypes?.map((contentType: ConfigContentType) => {
-                const ct: ContentType | undefined = allContentTypes?.find((item) => item.uid === contentType.uid)
-                if (!ct) return null
-                return (
-                  <Box
-                    borderColor={!contentType.default && 'danger500'}
-                    key={ct.uid}
-                  >
-                    <Accordion.Item key={ct.uid} value={ct.uid} size="S">
-                      <Accordion.Header>
-                        <Accordion.Trigger>
-                          {ct?.info.displayName}
-                        </Accordion.Trigger>
-                      </Accordion.Header>
-                      <Accordion.Content>
-                        <Box padding={3}>
-                          <Field.Root
-                            name="selectedContentTypes"
-                            hint={formatMessage({
-                              id: getTranslation('settings.page.defaultField.hint'),
-                              defaultMessage: 'The selected field from the content type will be used to generate the URL alias. Use a field that is unique and descriptive, such as a "title" or "name".',
-                            })}
-                            error={!contentType.default && formatMessage({
-                              id: getTranslation('settings.page.defaultField.error'),
-                              defaultMessage: 'Please select a default field',
-                            })}
-                            required
-                          >
-                            <Field.Label>
-                              {formatMessage({
-                                id: getTranslation('settings.page.defaultField'),
-                                defaultMessage: 'Default URL Alias field',
-                              })}
-                            </Field.Label>
-                            <SingleSelect
-                              name={`defaultField-${ct.uid}`}
-                              onClear={() => dispatch({ type: 'SET_DEFAULT_FIELD', payload: { ctUid: ct.uid, field: '' } })}
-                              value={contentType?.default || ''}
-                              onChange={(value: string) => dispatch({ type: 'SET_DEFAULT_FIELD', payload: { ctUid: ct.uid, field: value } })}
-                            >
-                              {Object.entries(ct.attributes).map(([key], index) => {
-                                if (
-                                  key === 'id' || 
-                                  key === 'documentId' ||
-                                  key === 'createdAt' || 
-                                  key === 'updatedAt' || 
-                                  key === 'createdBy' || 
-                                  key === 'updatedBy' ||
-                                  key === 'webatlas_path' ||
-                                  key === 'webatlas_override'
-                                ) return null
-                                return <SingleSelectOption key={index} value={key}>{key}</SingleSelectOption>
-                              })}
-                            </SingleSelect>
-                            <Field.Hint/>
-                          </Field.Root>
-                          <Box paddingTop={4}>
-                            <Field.Root
-                              name="urlAliasPattern"
-                              hint={formatMessage({
-                                id: getTranslation('settings.page.urlAliasPattern.hint'),
-                                defaultMessage: 'The pattern to prepend to the generated URL alias. For example, if you enter "blog" and the value of default field is "My First Post", the generated URL alias will be "blog/my-first-post". Leave empty for no prefix.',
-                              })}
-                            >
-                              <Field.Label>
-                                {formatMessage({
-                                  id: getTranslation('settings.page.urlAliasPattern'),
-                                  defaultMessage: 'URL Alias Pattern',
-                                })}
-                                <Tooltip description={formatMessage({
-                                  id: getTranslation('settings.page.urlAliasPattern.tooltip'),
-                                  defaultMessage: 'Leading and trailing slashes will be removed. Spaces will be replaced with hyphens. Special characters will be encoded.',
-                                })} />
-                              </Field.Label>
-                              <Field.Input
-                                value={contentType.pattern}
-                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => dispatch({ type: 'SET_PATTERN', payload: { ctUid: ct.uid, pattern: e.target.value } })}
-                                disabled={!contentType.default}
-                                type="text"
-                                placeholder={formatMessage({
-                                  id: getTranslation('settings.page.urlAliasPattern.placeholder'),
-                                  defaultMessage: 'e.g. blog',
-                                })}
-                              />
-                              <Field.Hint />
-                            </Field.Root>
-                          </Box>
-                        </Box>
-                      </Accordion.Content>
-                    </Accordion.Item>
-                  </Box>
-                )
+              {settingsState.selectedContentTypes?.map((contentTypeSettings: ConfigContentType) => {
+                const ct: ContentType | undefined = allContentTypes?.find((item) => item.uid === contentTypeSettings.uid)
+                return <ContentTypeAccordion contentType={ct} contentTypeSettings={contentTypeSettings} dispatch={dispatch} />
               })}
             </Accordion.Root>
           </Field.Root>
