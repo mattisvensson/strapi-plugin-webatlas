2aff88e48dd4763f9d386762d21a7ab5cdc6d20a
Matti Svensson <matti.s.58@web.de>
2026-01-12 19:58:35 +0100

    fix: move rbac check to router component

--- admin/src/pages/Navigation/index.tsx
+++ admin/src/pages/Navigation/index.tsx
@@ -1,12 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Navigation from './Page';
+import { Page } from '@strapi/strapi/admin';
+import pluginPermissions from '../../permissions';
+
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />} />
-      <Route path="/:navigationId" element={<Navigation />} />
-    </Routes>
+    <Page.Protect permissions={pluginPermissions['page.navigation']}>
+      <Routes>
+        <Route path="/" element={<Navigation />} />
+        <Route path="/:navigationId" element={<Navigation />} />
+      </Routes>
+    </Page.Protect>
   );
 };
 
