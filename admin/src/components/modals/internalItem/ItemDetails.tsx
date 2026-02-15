a88bbbffa6caa44b61485750b275634204017b0c
Matti Svensson <matti.s.58@web.de>
2026-02-15 11:35:52 +0100

    feat: implement ItemDetails component to handle route hierarchy on navigation items

---
+++ admin/src/components/modals/internalItem/ItemDetails.tsx
@@ -0,0 +1,139 @@
+import type { Route } from '../../../../../types';
+import type { ModalItem_VariantCreate } from '../../../types';
+import { Box, Grid, Field } from '@strapi/design-system';
+import PathInfo from '../../PathInfo';
+import { useEffect } from 'react';
+import { useModalSharedLogic } from '../useModalSharedLogic';
+import { useIntl } from 'react-intl';
+import { getTranslation } from '../../../utils';
+import Tooltip from '../../Tooltip';
+
+type ItemDetailsProps = Pick<ModalItem_VariantCreate & ReturnType<typeof useModalSharedLogic>, 'navItemState' | 'dispatchNavItemState' | 'path' | 'dispatchPath' | 'validationState' | 'debouncedCheckUrl'> & 
+  { route: Route, parentRoute: Route | null }
+
+export default function ItemDetails({ 
+  navItemState, 
+  dispatchNavItemState, 
+  path, 
+  dispatchPath, 
+  validationState, 
+  route, 
+  parentRoute,
+  debouncedCheckUrl
+}: ItemDetailsProps) {
+  const { formatMessage } = useIntl();
+
+  useEffect(() => {
+    if (path.needsUrlCheck && path.value) {
+      if (path.uidPath === path.value || path.initialPath === path.value) return
+			debouncedCheckUrl({ url: path.value, routeDocumentId: route.documentId, withoutTransform: true });
+			dispatchPath({ type: 'RESET_URL_CHECK_FLAG' });
+    }
+  }, [path.needsUrlCheck, route.documentId]);
+
+  useEffect(() => {
+    if (!path.slug) return
+    const newPath = parentRoute ? `${parentRoute.canonicalPath}/${path.slug}` : path.slug
+    dispatchPath({ type: 'DEFAULT', payload: newPath })
+  }, [path.slug, parentRoute])
+
+  return (
+    <Grid.Root gap={4}>
+      <Grid.Item col={12} s={12} alignItems="baseline">
+        <Box width="100%">
+          <Field.Root required>
+            <Field.Label>
+              {formatMessage({
+                id: getTranslation('modal.item.titleField.label'),
+                defaultMessage: 'Title'
+              })}
+            </Field.Label>
+            <Field.Input
+              name="title"
+              value={navItemState?.title || ''}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => dispatchNavItemState({ type: 'SET_TITLE', payload: e.target.value })}
+              required
+            />
+          </Field.Root>
+        </Box>
+      </Grid.Item>
+      <Grid.Item col={6} s={12} alignItems="baseline">
+        <Box width="100%">
+          <Field.Root>
+            <Field.Label>
+              {formatMessage({
+                id: getTranslation('modal.item.canonicalPathField.label'),
+                defaultMessage: 'Canonical Path'
+              })}
+              <Tooltip description={formatMessage({
+                id: getTranslation('modal.item.canonicalPathField.tooltip'),
+                defaultMessage: 'Based on content hierarchy'
+              })} />
+            </Field.Label>
+            <Field.Input
+              name="canonicalPath"
+              value={route.canonicalPath || ''}
+              disabled
+            />  
+            <Field.Hint />  
+          </Field.Root>
+        </Box>
+      </Grid.Item>
+      <Grid.Item col={6} s={12} alignItems="baseline">
+        <Box width="100%">
+          <Field.Root required>
+            <Field.Label>
+              {formatMessage({
+                id: getTranslation('modal.item.slugField.label'),
+                defaultMessage: 'Slug'
+              })}
+            </Field.Label>
+            <Field.Input
+              name="slug"
+              value={path.slug}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => dispatchPath({ type: 'SET_SLUG', payload: e.target.value })}
+              onBlur={(e: React.ChangeEvent<HTMLInputElement>) => {
+                dispatchPath({ type: 'SET_SLUG', payload: e.target.value })}
+              }
+            />
+          </Field.Root>
+        </Box>
+      </Grid.Item>
+      <Grid.Item col={6} s={12} alignItems="baseline">
+        <Box width="100%">
+          <Field.Root>
+            <Field.Label>
+              {formatMessage({
+                id: getTranslation('modal.item.navigationPosition.label'),
+                defaultMessage: 'Navigation Position'
+              })}
+            </Field.Label>
+            <Field.Input
+              name="navigationPosition"
+              value={parentRoute?.title || 'Root'}
+              disabled
+            />
+          </Field.Root>
+        </Box>
+      </Grid.Item>
+      <Grid.Item col={6} s={12}>
+        <Box width="100%">
+          <Field.Root>
+            <Field.Label>
+              {formatMessage({
+                id: getTranslation('modal.item.pathField.label'),
+                defaultMessage: 'Path'
+              })}
+            </Field.Label>
+            <Field.Input
+              name="path"
+              value={path.value}
+              disabled
+            />
+          </Field.Root>
+          <PathInfo validationState={validationState} replacement={path.replacement} />
+        </Box>
+      </Grid.Item>
+    </Grid.Root>
+  )
+}
