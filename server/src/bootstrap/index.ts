83cb3dbb22bd8ce2decbc3c8fcc85ad7ae10481a
Matti Svensson <matti.s.58@web.de>
2026-04-28 22:11:27 +0200

    fix: refactor middleware structure and replace contentTypeMiddleware with sanitizeWebatlas middleware

--- server/src/bootstrap/index.ts
+++ server/src/bootstrap/index.ts
@@ -2,12 +2,10 @@ import { registerPermissions } from "./permissions";
 import { syncConfig } from "./syncConfig";
 import { documentMiddleware } from "./documentMiddleware";
 import { webatlasMiddleware } from "./webatlasMiddleware";
-import { contentTypeMiddleware } from "./contentTypeMiddleware";
 
 export {
   registerPermissions,
   syncConfig,
   documentMiddleware,
   webatlasMiddleware,
-  contentTypeMiddleware,
 }
