b05d95b1576b8402d425aff3784c78ff389f2b78
Matti Svensson <matti.s.58@web.de>
2026-05-03 12:49:33 +0200

    refactor: remove webatlas field settings from register.ts

--- server/src/register.ts
+++ server/src/register.ts
@@ -1,26 +1,3 @@
-import set from 'lodash/set'
 import { Core } from '@strapi/strapi'
-import type { ContentType } from '../../types'
 
-export default ({ strapi }: { strapi: Core.Strapi }) => {
-	Object.values(strapi.contentTypes).forEach((contentType: ContentType) => {
-		// Only add fields to content types that have webatlas enabled in plugin options
-		if (!contentType.pluginOptions?.webatlas?.enabled) return
-
-		const { attributes } = contentType
-
-		const fieldSettings = {
-			writable: true,
-			configurable: false,
-			editable: false,
-			visible: true,
-			default: null,
-		}
-
-		set(attributes, 'webatlas', {
-			...fieldSettings,
-			type: 'json',
-			private: false,
-		})
-	})
-}
+export default ({ strapi }: { strapi: Core.Strapi }) => {}
